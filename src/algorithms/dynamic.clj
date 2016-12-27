74ac39bcb3c0f2fbef54a2cdd133088ed1b099c0
Rohit Thadani <rthadani@fanatics.com>
2016-12-27 14:15:41 -0800

    dynamic

--- src/algorithms/dynamic.clj
+++ src/algorithms/dynamic.clj
@@ -67,6 +67,9 @@
   [jobs]
   (sort-by #(second %) jobs))
 
+;;weighted job indexes
+;;opt(j) = max(opt(j-1), v(n) + opt(p(j)))
+;where p(j) = the righttmost interval before j that does not overlap with j -1 if no interval found
 (defn make-job-indexes
   [jobs]
   (let [sorted-jobs (sort-by second jobs)
@@ -75,25 +78,77 @@
                             j (range 0 (count sorted-jobs))
                             :let [[si fi _] (nth sorted-jobs i)
                                   [sj fj _] (nth sorted-jobs j)]
-                            :when (< fj si)]
+                            :when (<= fj si)]
                         [i j]))
         missing-keys (set/difference (into #{} (range 0 (count sorted-jobs))) (keys indexed))
         indexed-missing-keys (into {} (map (fn [key] [key -1]) missing-keys))]
-    [sorted-jobs (merge indexed indexed-missing-keys {-1 0})]))
+    [sorted-jobs (merge indexed indexed-missing-keys {-1 -1})]))
 
 (declare wis)
+
 (defn wis*
-  [jobs job-indexes j]
+  [jobs job-indexes j soln]
   (if (= -1 j)
     0
-    (let [[_ _ v] (nth jobs j)]
-     (max (+ v (wis jobs job-indexes (get job-indexes j)))
-          (wis jobs job-indexes (dec j))))))
+    (let [[_ _ v] (nth jobs j)
+          value-j (max (+ v (wis jobs job-indexes (get job-indexes j) soln))
+                       (wis jobs job-indexes (dec j) soln))]
+      (swap! soln assoc j value-j)
+      value-j)))
 
 (def wis (memoize wis*))
 
+(defn print-solution-wis
+  [j jobs job-indexes soln]
+  (when (not= j -1)
+    (let [[_ _ v] (nth jobs j)
+        valuepj (if (= -1 (get job-indexes j)) 0 (get soln (get job-indexes j)))
+        valuej-1 (if (< (dec j) 0) 0 (get soln (dec j)))   ]
+      (if (> (+ v valuepj)  valuej-1)
+        (do
+          (println j)
+          (print-solution-wis (get job-indexes j) jobs job-indexes soln))
+        (print-solution-wis (dec j) jobs job-indexes soln)))))
+
 (defn weighted-interval-schedule
   [jobs]
-  (let [ [sorted-jobs pjs](make-job-indexes jobs)]
-    (wis sorted-jobs pjs (dec (count sorted-jobs)))))
+  (let [[sorted-jobs pjs] (make-job-indexes jobs)
+        soln (atom (vec (range 0 (count jobs))))]
+    (wis sorted-jobs pjs (dec (count sorted-jobs)) soln)
+    (println soln)))
 
+
+
+
+#_(weighted-interval-schedule [[1 2 50]
+                               [3 5 20]
+                               [6 19 100]
+                 amaz              [2 100 200]])
+
+#_(weighted-interval-schedule-with-soln [[1 2 50]
+                                         [3 5 20]
+                                         [6 19 100]
+                                         [2 100 200]])
+
+;;Max Subset sum
+;If w < wi then opt(i, w) = opt(i - 1 ,w)
+; else Opt(i,w) = max (Opt(i-i, w), w(i) + Opt(i - 1, w - wi))
+(declare subset-sum)
+(defn subset-sum*
+  [i weights w]
+  (cond
+    (< i 0) [0 []]
+    (<= w 0) [0 []]
+    :else
+    (let [wi (weights i)]
+      (if (> wi w)
+        (subset-sum (dec i) weights w)
+        (let [[v-withouti items-witouti :as without-i] (subset-sum (dec i) weights w)
+              [v-with-i items-with-i as :as with-i] (subset-sum (dec i) weights (- w wi))]
+          (if (> (+ v-with-i wi) v-withouti)
+            [(+ v-with-i wi) (conj items-with-i i)]
+            without-i ))))))
+
+(def subset-sum (memoize subset-sum*))
+#_(def soln (atom (vec (range 0 6))))
+#_(subset-sum 5 [3 34 4 12 5 2] 7)
