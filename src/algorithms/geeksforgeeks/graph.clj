b61e5b531852cf27f9212503a22b326d22d07714
Rohit Thadani <rthadani@fanatics.com>
2017-05-12 10:19:58 -0700

    topological sort

--- src/algorithms/geeksforgeeks/graph.clj
+++ src/algorithms/geeksforgeeks/graph.clj
@@ -34,3 +34,53 @@
         :2 [:4]
         :3 [:4]
         :4 [:1]} :1)
+
+
+;;topological sort
+;;assumes agraph with no cycles
+(defn in-degree
+  [graph]
+  (reduce
+    (fn [degree [node outgoing]]
+      (as-> (update degree node (fnil identity 0)) $
+            (reduce (fn [degree node] (update degree node (fnil inc 0))) $ outgoing)))
+    {}
+    graph))
+
+(defn decrease-in-degree
+  [graph node in-degree]
+  (reduce
+    (fn [in-degree node]
+      (update in-degree node dec))
+    in-degree
+    (get graph node)))
+
+(defn get-new-root-nodes
+  [in-degree]
+  (keys (filter (fn [[_ degree]] (zero? degree)) in-degree)))
+
+(defn filter-0-indegree
+  [in-degree]
+  (into {} (filter (fn [[_ degree]] (not (zero? degree))) in-degree)))
+
+(defn topological-sort
+  [graph]
+  (let [in-degree (in-degree graph)
+        queue     (into (PersistentQueue/EMPTY) (get-new-root-nodes in-degree))]
+    (loop [q   queue
+           i   (filter-0-indegree in-degree)
+           acc []]
+      (if (empty? q)
+        acc
+        (let [node          (peek q)
+              new-in-degree (decrease-in-degree graph node i)
+              new-roots     (get-new-root-nodes new-in-degree)]
+          (recur (into (pop q) new-roots)
+                 (filter-0-indegree new-in-degree)
+                 (conj acc (peek q))))))))
+
+#_(topological-sort {:5 [:2 :0]
+                     :2 [:3]
+                     :4 [:0 :1]
+                     :3 [:1]
+                     :0 []})
