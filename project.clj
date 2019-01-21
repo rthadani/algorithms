5f7294a76d0e9e8169b8f0654e4b657c0ddf1c3a
rthadani <rohit.thadani@gmail.com>
2019-01-21 08:41:02 -0800

    closest-pair

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [cheshire "5.5.0"]
--- src/algorithms/stanford/closest_pair.clj
+++ src/algorithms/stanford/closest_pair.clj
@@ -5,7 +5,7 @@
 
 (defn get-pair
   [points]
-  (closest-pair (sort-by :x points) (sort-by :y points)))
+  (closest-pair (sort-by :x points)))
 
 (defn euclid-distance
   [p1 p2]
@@ -13,6 +13,39 @@
       (+ (Math/pow (- (:y p1) (:y p2)) 2))
       Math/sqrt))
 
+(defn brute-force
+  [px]
+  (min (for [i (range 0 (count px))
+             j (range i (count px))]
+         (euclid-distance (px i) (px j)))))
+
+(defn- mid-strip
+  [px delta]
+  (let [mid (int (/ (count px) 2))
+        mid-x (:x (px mid))  ]
+    (vec (filter (fn [{:keys [x]}] (< delta (Math/abs (- x mid-x)) )) px))))
+
+(defn- min-instrip-distance
+  [strip delta]
+  (let [sy (vec (sort-by :y strip))
+        min-distance (for [i (range 0 (count sy))
+                           j (range i (count sy))
+                           :let [{:keys [yi]} (sy i)
+                                 {:keys [yj]} (sy j)
+                                 dist (Math/abs (- yi yj))]
+                           :when (< dist delta)]
+                       dist)]
+    (if (empty? min-distance)
+      Integer/MAX_VALUE
+      (apply min min-distance))))
+
+
 (defn- closest-pair
-  [px py]
-  )
+  [px]
+  (if (<= (count px) 3) 
+    (brute-force px)
+    (let [mid (int (/ (count px) 2))
+          dl (closest-pair (subvec px 0 mid))
+          dr (closest-pair (subvec px mid (count px)))
+          split-strip (mid-strip px (min dl dr))]
+      (min dl dr (min-instrip-distance split-strip (min dl dr))))))
