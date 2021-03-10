1ebac8a52a18ac856c8e8f23e78a79b6700f039c
rthadani <rohit.thadani@gmail.com>
2021-03-10 07:05:10 -0800

    Added dbug thread macro

---
+++ src/algorithms/debug.clj
@@ -0,0 +1,52 @@
+(ns algorithms.debug)
+
+(defmacro verbose->>
+  [x & forms]
+  (loop [x x
+         forms forms
+         intermediate []]
+    (if forms
+      (let [form (first forms)
+            threaded (if (seq? form)
+                       (with-meta `(~(first form) ~@(next form) ~x) (meta form))
+                       (list form x))]
+        (recur
+          threaded
+          (next forms)
+          (conj intermediate `(do
+                                (println "x: ")
+                                (pp/pprint ~x)
+                                (println "\nthreaded: ")
+                                (pp/pprint '~threaded)
+                                (println "\n=> ")
+                                (pp/pprint ~threaded)
+                                (println "\n-------")))))
+      (list 'do
+        `(doseq [i# ~intermediate]
+           i#)
+        x))))
+
+
+(defmacro verbose->
+  {:added "1.0"}
+  [x & forms]
+  (loop [x x, forms forms, intermediate []]
+    (if forms
+      (let [form (first forms)
+            threaded (if (seq? form)
+                       (with-meta `(~(first form) ~x ~@(next form)) (meta form))
+                       (list form x))]
+        (recur threaded
+               (next forms)
+               (conj intermediate `(do
+                                     (println "x: ")
+                                     (pp/pprint ~x)
+                                     (println "\nthreaded: ")
+                                     (pp/pprint '~threaded)
+                                     (println "\n=> ")
+                                     (pp/pprint ~threaded)
+                                     (println "\n-------")))))
+      (list 'do
+            `(doseq [i# ~intermediate]
+               i#) 
+      x))))
